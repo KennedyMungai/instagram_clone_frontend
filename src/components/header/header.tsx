83ebf787d94f2d0a6c3eb180b6374d4eb23b5ebe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 10:55:28 +0300

    Updated the styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -34,7 +34,7 @@ const Header = (props: Props) => {
 				<input
 					type='text'
 					placeholder='Search'
-					className='bg-gray-200 w-full p-2 rounded-md placeholder:text-center'
+					className='bg-gray-50 w-full p-2 rounded-md placeholder:text-center'
 				/>
 			</div>
 			{/* <Right></Right> */}
