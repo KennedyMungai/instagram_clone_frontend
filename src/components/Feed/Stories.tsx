dc31f8d21fdd4f2874aa0d310f081ca21b3a6479
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 12:31:42 +0300

    Defined the story users

--- src/components/Feed/Stories.tsx
+++ src/components/Feed/Stories.tsx
@@ -12,9 +12,12 @@ const Stories = (props: Props) => {
       const storyUsers = minifaker.array(25, (i) => (
         {
             username: minifaker.username({locale: 'en'}),
-            img: `https://i.pravatar.cc/150?img=${Math.ceil(Math.random * 70)}`
+            img: `https://i.pravatar.cc/150?img=${Math.ceil(Math.random * 70)}`,
+            id: i
         }
       ))
+
+      setStoryUsers(storyUsers)
     }, [])
     
 
