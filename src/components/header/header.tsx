e99ca29e359933253c64f395fbf57739060125e0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 09:54:02 +0300

    Defined the css classes for an inner div inside the header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,13 +7,14 @@ const Header = (props: Props) => {
 		<div>
 			{/* Left */}
 			<div className=''>
-				<div className=''>
-                    <Image 
-                    src='/Images/InstagramLogo.png' 
-                    width={512} 
-                    height={512} 
-                    alt="Instagram Logo" 
-                    />
+				<div className='h-24 w-24 relative hidden lg:inline-grid'>
+					<Image
+						src='/Images/InstagramLogo.png'
+						width={512}
+						height={512}
+						alt='Instagram Logo'
+						className='object-contain'
+					/>
 				</div>
 			</div>
 			{/* Middle */}
