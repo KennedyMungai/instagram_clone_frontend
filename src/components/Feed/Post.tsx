e3d46449b379c901050e10e254e0fc01ed283e27
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 15:30:07 +0300

    More updates to the markup

--- src/components/Feed/Post.tsx
+++ src/components/Feed/Post.tsx
@@ -10,15 +10,30 @@ type Props = {
 
 const Post = ({ username, title, userimage, img, caption }: Props) => {
 	return (
-		<div className='bg-white p-2 my-3 '>
-			<h2 className='text-2xl'>{title}</h2>
+		<div className='bg-white p-2 my-3'>
+			<div className='flex flex-row justify-between py-3'>
+				<div className='flex flex-row items-center gap-1 px-1'>
+					<Image
+						src={userimage}
+						width={50}
+						height={50}
+						alt={username}
+						className='rounded-full'
+					/>
+					<p className='text-sm font-bold'>{username}</p>
+				</div>
+				<div className=''>
+					<p className='cursor-pointer'>...</p>
+				</div>
+			</div>
 			<Image
 				src={img}
 				width={500}
 				height={500}
 				alt='title'
-				className='rounded-lg'
+				className='rounded-lg w-full'
 			/>
+			<h2 className='text-2xl'>{title}</h2>
 		</div>
 	)
 }
