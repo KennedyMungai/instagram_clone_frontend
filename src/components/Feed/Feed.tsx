83e785ecc595587a6c238f8fd27b7bf1fd2e4173
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 15:29:29 +0300

    Properly spaced out different parts of the page

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -6,14 +6,14 @@ type Props = {}
 
 const Feed = (props: Props) => {
 	return (
-		<section className='flex flex-row mx-10 justify-between'>
-			<div className='max-w-[55%]'>
+		<section className='flex flex-row mx-20 justify-between'>
+			<div className='max-w-[60%]'>
 				{/* Stories */}
 				<Stories />
 				{/* Posts */}
 				<Posts />
 			</div>
-			<div className='hidden md:flex max-w-[40%]'>
+			<div className='hidden md:flex max-w-[35%]'>
 				{/* Mini Profile */}
 
 				{/* Suggestions */}
