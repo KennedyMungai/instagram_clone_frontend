5488e1032a5f9a28db66081afb7bb0afb79b6b57
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 14:10:57 +0300

    Hid the x axis overflow of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/header/header'
 
 export default function Home() {
 	return (
-		<main className='min-h-screen bg-slate-100'>
+		<main className='min-h-screen bg-slate-100 overflow-x-hidden'>
 			{/* Header */}
 			<Header />
 
