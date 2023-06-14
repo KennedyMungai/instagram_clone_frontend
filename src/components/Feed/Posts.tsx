68d9e4ca5fc196d0c0581aac0622be9b62d318e7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 16:19:08 +0300

    Added the number of dummy posts

--- src/components/Feed/Posts.tsx
+++ src/components/Feed/Posts.tsx
@@ -21,6 +21,94 @@ const Posts = (props: Props) => {
 			userimage: '/Images/Profile.jpg',
 			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
 			caption: 'Lit like a MF'
+		},
+		{
+			id: '3',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '4',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '5',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '6',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '7',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '8',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '9',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '10',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '11',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '12',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
+		},
+		{
+			id: '13',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
 		}
 	]
 
