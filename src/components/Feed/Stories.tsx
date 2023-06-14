012365dfd64cc93c5c8e256f0e567d2a15c357cb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 12:54:26 +0300

    Mapped out the users for the story component

--- src/components/Feed/Stories.tsx
+++ src/components/Feed/Stories.tsx
@@ -2,32 +2,46 @@
 import "minifaker/locales/en"
 import minifaker from 'minifaker'
 import { useState, useEffect } from 'react'
+import Story from './Story'
 
 type Props = {}
- 
+
+type storyUsers = {
+	username: string
+	img: string
+	id: number
+}
+
 const Stories = (props: Props) => {
-	const [storyUsers, setStoryUsers] = useState([])
+	const [storyUsers, setStoryUsers] = useState<storyUsers[]>([])
 
-    useEffect(() => {
-      const storyUsers = minifaker.array(25, (i) => (
-        {
-            username: minifaker.username({locale: 'en'}).toLowerCase(),
-            img: `https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`,
-            id: i
-        }
-      ))
+	useEffect(() => {
+		const storyUsers = minifaker.array(25, (i) => ({
+			username: minifaker.username({ locale: 'en' }).toLowerCase(),
+			img: `https://i.pravatar.cc/150?img=${Math.ceil(
+				Math.random() * 70
+			)}`,
+			id: i
+		}))
 
-      setStoryUsers(storyUsers)
+		setStoryUsers(storyUsers)
 
-      console.log(storyUsers)
-    }, [])
-    
+		console.log(storyUsers)
+	}, [])
 
-	return <div>
-        {storyUsers.map((user) => {
-            return 
-        })}
-    </div>
+	return (
+		<div>
+			{storyUsers.map((user) => {
+				return (
+					<Story
+						username={user.username}
+						img={user.img}
+						key={user.id}
+					/>
+				)
+			})}
+		</div>
+	)
 }
 
 export default Stories
