8dbeb778b2b475950e23ca0b2b482c14d480ddcc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 14:17:48 +0300

    Some changes were made

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 const Header = (props: Props) => {
 	return (
-		<nav className='shadow-md border-b sticky top-0 bg-white mb-10 z-30'>
+		<nav className='shadow-md border-b sticky top-0 bg-white mb-10 z-30 max-w-screen'>
 			<div className='flex flex-row justify-around items-center max-w-6xl m-2 md:m-5'>
 				{/* Left */}
 				<div className='h-24 w-24 cursor-pointer hidden md:block'>
