4e811111bf31e0d7fca8b3787ba3681d4e98869a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 14:49:41 +0300

    Added another object to the posts array

--- src/components/Feed/Posts.tsx
+++ src/components/Feed/Posts.tsx
@@ -10,6 +10,14 @@ const Posts = (props: Props) => {
 			userimage: '/Images/Profile.jpg',
 			img: 'https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
 			caption: 'Lit like a MF'
+		},
+		{
+			id: '2',
+			username: 'ChickenWings1',
+			title: 'Something about rich daddies',
+			userimage: '/Images/Profile.jpg',
+			img: 'https://images.unsplash.com/photo-1662010021854-e67c538ea7a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			caption: 'Lit like a MF'
 		}
 	]
 
