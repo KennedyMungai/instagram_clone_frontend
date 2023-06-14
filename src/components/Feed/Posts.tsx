b846225036c6837115029e126f9805d618f3bd4c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 14:43:30 +0300

    Defined some sample data

--- src/components/Feed/Posts.tsx
+++ src/components/Feed/Posts.tsx
@@ -2,6 +2,15 @@
 type Props = {}
 
 const Posts = (props: Props) => {
+    const posts = [
+		{
+			id: '1',
+			username: 'ChickenWings1',
+			title: 'Post 1',
+			userimage: 'https://i.pravatar.cc/150?img=53'
+		}
+	]
+
 	return <div>Posts</div>
 }
 
