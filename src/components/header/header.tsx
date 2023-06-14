753ddef51a17a9f4393340de9e4d7b74a25eade0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 10:38:56 +0300

    Updated the markup for the header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,36 +1,35 @@
 import Image from 'next/image'
+import { HiMagnifyingGlass } from 'react-icons/hi2'
 
 type Props = {}
 
 const Header = (props: Props) => {
 	return (
-		<div>
+		<div className='flex flex-row justify-around items-start max-w-6xl'>
 			{/* Left */}
-			<div className='flex flex-row justify-around items-start max-w-6xl'>
-				<div className='h-24 w-24 cursor-pointer hidden md:block'>
-					<Image
-						src='/Images/InstagramText.png'
-						width={512}
-						height={512}
-						alt='Instagram Logo'
-						className='object-contain'
-					/>
-				</div>
-				<div className='h-24 w-24 md:hidden cursor-pointer'>
-					<Image
-						src='/Images/InstagramLogo.png'
-						width={512}
-						height={512}
-						alt='Instagram Logo'
-						className='object-contain'
-					/>
-				</div>
-				<h1>Chicken Wings</h1>
-				<h1>Chicken Wings</h1>
+			<div className='h-24 w-24 cursor-pointer hidden md:block'>
+				<Image
+					src='/Images/InstagramText.png'
+					width={512}
+					height={512}
+					alt='Instagram Logo'
+					className='object-contain'
+				/>
+			</div>
+			<div className='h-24 w-24 md:hidden cursor-pointer'>
+				<Image
+					src='/Images/InstagramLogo.png'
+					width={512}
+					height={512}
+					alt='Instagram Logo'
+					className='object-contain'
+				/>
 			</div>
 			{/* Middle */}
+			<h1>Chicken Wings</h1>
 
 			{/* <Right></Right> */}
+			<h1>Chicken Wings</h1>
 		</div>
 	)
 }
