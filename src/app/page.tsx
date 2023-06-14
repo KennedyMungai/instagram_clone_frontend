d17dcf52c678a17e3f60b0cea51601d66d0f638b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 09:16:59 +0300

    Updated the color of the h1 text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<h1 className='flex align-center justify-center font-bold text-3xl'>
+			<h1 className='flex align-center justify-center font-bold text-3xl text-rose-500'>
 				Instagram Bitches
 			</h1>
 		</main>
