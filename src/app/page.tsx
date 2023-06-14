cd81986609bbd8a4a166cc130e0e18b8b6467f5e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 09:41:04 +0300

    Added the header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import Image from 'next/image'
+import Header from '@/components/header/header'
 
 export default function Home() {
-  return (
+	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<h1 className='flex align-center justify-center font-bold text-3xl text-rose-500'>
 				Instagram Bitches
 			</h1>
 
 			{/* Header */}
+			<Header />
 
 			{/* Feeds */}
 
 			{/* Modal */}
 		</main>
-  )
+	)
 }
