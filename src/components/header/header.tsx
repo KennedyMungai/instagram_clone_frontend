7b2eaf96f267b91610486f30edc2a040708704f3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 09:49:42 +0300

    Added the instagram logo to the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image'
+
 type Props = {}
 
 const Header = (props: Props) => {
-	return <div>header</div>
+	return (
+		<div>
+			{/* Left */}
+			<div className=''>
+				<div className=''>
+                    <Image 
+                    src='/Images/InstagramLogo.png' 
+                    width={512} 
+                    height={512} 
+                    alt="Instagram Logo" 
+                    />
+				</div>
+			</div>
+			{/* Middle */}
+
+			{/* <Right></Right> */}
+		</div>
+	)
 }
 
 export default Header
