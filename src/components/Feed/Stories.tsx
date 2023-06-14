43c92e53434accc5de272d4d55895e618b11ddf2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 12:18:30 +0300

    Made use effect a stories component

--- src/components/Feed/Stories.tsx
+++ src/components/Feed/Stories.tsx
@@ -1,10 +1,11 @@
+'use effect'
 import minifaker from 'minifaker'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 type Props = {}
 
 const Stories = (props: Props) => {
-    const [storyUsers, setStoryUsers] = useState([])
+	const [storyUsers, setStoryUsers] = useState([])
 
 	return <div>Stories</div>
 }
