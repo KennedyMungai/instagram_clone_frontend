aa4241b7c63ee64485dd0fe8af0474af315e6376
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 11:52:48 +0300

    Made the header sticky and have a background of white

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 const Header = (props: Props) => {
 	return (
-		<div className='shadow-md border-b sticky top-0'>
+		<div className='shadow-md border-b sticky top-0 bg-white mb-10'>
 			<div className='flex flex-row justify-around items-center max-w-6xl m-2 md:m-5'>
 				{/* Left */}
 				<div className='h-24 w-24 cursor-pointer hidden md:block'>
