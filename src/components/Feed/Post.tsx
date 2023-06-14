2d0e85ffa4e0b240859dc7ff05270ec6698357ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 14:54:25 +0300

    Destructured the code for the post component

--- src/components/Feed/Post.tsx
+++ src/components/Feed/Post.tsx
@@ -1,6 +1,12 @@
-type Props = {}
+type Props = {
+    username: string,
+    title: string,
+    userImage: string,
+    img: string,
+    caption: string
+}
 
-const Post = (props: Props) => {
+const Post = ({username, title, userImage, img, caption}: Props) => {
 	return <div>Post</div>
 }
 
