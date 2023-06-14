bb57c8d01836311f9a6eb37f1dc89de344dd3ba6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 15:04:46 +0300

    Defined the widths of the two divs

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -7,13 +7,13 @@ type Props = {}
 const Feed = (props: Props) => {
 	return (
 		<section className='flex flex-row mx-10 justify-between'>
-			<div className=''>
+			<div className='max-w-[55%]'>
 				{/* Stories */}
 				<Stories />
 				{/* Posts */}
 				<Posts />
 			</div>
-			<div className='hidden md:flex'>
+			<div className='hidden md:flex max-w-[45%]'>
 				{/* Mini Profile */}
 
 				{/* Suggestions */}
