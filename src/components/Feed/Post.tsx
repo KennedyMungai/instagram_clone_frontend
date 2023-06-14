7fa353d7f2cb429e4c8b355fe99641f9c5b44a00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 15:02:16 +0300

    Some simple markup

--- src/components/Feed/Post.tsx
+++ src/components/Feed/Post.tsx
@@ -1,13 +1,20 @@
+import Image from 'next/image'
+
 type Props = {
-    username: string,
-    title: string,
-    userimage: string,
-    img: string,
-    caption: string
+	username: string
+	title: string
+	userimage: string
+	img: string
+	caption: string
 }
 
-const Post = ({username, title, userimage, img, caption}: Props) => {
-	return <div>Post</div>
+const Post = ({ username, title, userimage, img, caption }: Props) => {
+	return (
+		<div className='flex flex-col'>
+			<p className='text-right'>{title}</p>
+			<Image src={img} width={500} height={500} alt='title' />
+		</div>
+	)
 }
 
 export default Post
