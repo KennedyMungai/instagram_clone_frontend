3d2d6c8e558c987973a73b6fec77ef41aa4596af
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 09:14:48 +0300

    Switched from using inter to montserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
 	title: 'Instagram Clone',
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+		<html lang='en'>
+			<body className={montserrat.className}>{children}</body>
+		</html>
   )
 }
