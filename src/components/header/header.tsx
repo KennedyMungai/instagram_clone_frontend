758ee0f201094b7fd038cf039b6c67802854d36c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-14 09:59:16 +0300

    Added a pciture of the instagram text

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,9 +7,16 @@ const Header = (props: Props) => {
 		<div>
 			{/* Left */}
 			<div className=''>
-				<div className='h-24 w-24 relative hidden lg:inline-grid'>
+				<div className='h-24 w-24 relative hidden md:inline-grid cursor-pointer'>
 					<Image
 						src='/Images/InstagramLogo.png'
+						width={512}
+						height={512}
+						alt='Instagram Logo'
+						className='object-contain'
+					/>
+					<Image
+						src='/Images/InstagramText.png'
 						width={512}
 						height={512}
 						alt='Instagram Logo'
