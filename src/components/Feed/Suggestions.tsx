68abc88fa0efbe2001ece9393f750dafbe1a68d0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 10:58:38 +0300

    Added an interface to help with the type definitions

--- src/components/Feed/Suggestions.tsx
+++ src/components/Feed/Suggestions.tsx
@@ -1,12 +1,20 @@
 'use client'
 import minifaker from 'minifaker'
 import 'minifaker/locales/en'
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 
 type Props = {}
 
+interface ISuggestion {
+	username: string
+	jobTitle: string
+	avatar: string
+	id: number
+}
+
 const Suggestions = (props: Props) => {
-	const [suggestions, setSuggestions] = useState([])
+	const [suggestions, setSuggestions] = useState<ISuggestion[]>([])
 
 	useEffect(() => {
 		const sample_suggestions = minifaker.array(5, (i) => ({
@@ -29,6 +37,17 @@ const Suggestions = (props: Props) => {
 				<h3 className='font-bold text-gray-400'>Suggestions For You</h3>
 				<button className='text-gray-600 font-semibold'>See All</button>
 			</div>
+			{suggestions.map((suggestion) => (
+				<div className='' key={suggestion.id}>
+					<Image
+						src={suggestion.avatar}
+						width={50}
+						height={50}
+						className='rounded-full border p-[2px]'
+						alt={suggestion.username}
+					/>
+				</div>
+			))}
 		</div>
 	)
 }
