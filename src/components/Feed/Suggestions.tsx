bc243d695dce52e81e48beb7eeb1db283aacd057
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 10:32:45 +0300

    Added some text and styled it

--- src/components/Feed/Suggestions.tsx
+++ src/components/Feed/Suggestions.tsx
@@ -2,7 +2,14 @@
 type Props = {}
 
 const Suggestions = (props: Props) => {
-	return <div>Suggestions</div>
+    return (
+		<div>
+			<div className='flex flex-row justify-between mb-5 text-sm'>
+				<h3 className='font-bold text-gray-400'>Suggestions For You</h3>
+				<button className='text-gray-600 font-semibold'>See All</button>
+			</div>
+		</div>
+	)
 }
 
 export default Suggestions
