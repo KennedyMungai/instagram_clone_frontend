47db07a616280dd2228c342967239b221a386769
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 09:38:24 +0300

    Updated the sizes of the two sections

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -7,13 +7,13 @@ type Props = {}
 const Feed = (props: Props) => {
 	return (
 		<section className='flex flex-row md:mx-20 justify-between mx-4'>
-			<div className='md:max-w-[60%] w-full'>
+			<div className='md:max-w-[65%] w-full'>
 				{/* Stories */}
 				<Stories />
 				{/* Posts */}
 				<Posts />
 			</div>
-			<div className='hidden md:flex max-w-[35%]'>
+			<div className='hidden md:flex max-w-[30%]'>
 				{/* Mini Profile */}
 
 				{/* Suggestions */}
