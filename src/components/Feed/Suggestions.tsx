41656c8f0a2496347e50691f41c2196b822ceb26
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-06-15 10:41:31 +0300

    Created the suggesntions page

--- src/components/Feed/Suggestions.tsx
+++ src/components/Feed/Suggestions.tsx
@@ -1,10 +1,13 @@
 'use client'
 import minifaker from 'minifaker'
+import { useState } from 'react'
 
 type Props = {}
 
 const Suggestions = (props: Props) => {
-    return (
+	const [suggestions, setSuggestions] = useState([])
+
+	return (
 		<div>
 			<div className='flex flex-row justify-between mb-5 text-sm'>
 				<h3 className='font-bold text-gray-400'>Suggestions For You</h3>
